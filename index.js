87f4d5842758d0cdf209187b8a380da3c651b4fc
Jonathan Stassen <godlvall2jm@gmail.com>
2019-01-30 01:40:32 -0600

    Cleanup and document

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
 #!/usr/bin/env node
 var program = require('commander');
+var fs = fs || require('fs');
 
 const basedir = process.cwd();
 
 program
-    .version('0.1.0')
-    .option('-d, --destination [destination]', 'Destination folder')
-    .parse(process.argv);
+	.version('0.1.0')
+	.option('-d, --destination [destination]', 'Destination folder')
+	.parse(process.argv);
 
-var fs = fs || require('fs')
-
-var walkSync = function (dir, filelist) {
-    var fs = fs || require('fs'),
-        files = fs.readdirSync(dir);
-    filelist = filelist || [];
-    files.forEach(function (file) {
-        if (fs.statSync(dir + '/' + file).isDirectory()) {
-            filelist = walkSync(dir + '/' + file, filelist);
-        }
-        else {
-            const newdir = dir.replace(basedir, program.destination)
-            filelist.push(newdir + '/' + file);
-        }
-    });
-    return filelist;
+// Build a list of all files recursevly in a directory
+const walkSync = function (dir, filelist) {
+	var fs = fs || require('fs');
+	const files = fs.readdirSync(dir);
+	filelist = filelist || [];
+	files.forEach(function (file) {
+		if (fs.statSync(dir + '/' + file).isDirectory()) {
+			filelist = walkSync(dir + '/' + file, filelist);
+		}
+		else {
+			const newdir = dir.replace(basedir, '')
+			filelist.push([newdir, file]);
+		}
+	});
+	return filelist;
 };
 
-const filepaths = walkSync(process.cwd());
-
-if (!fs.existsSync(program.destination)) {
-    fs.mkdirSync(program.destination);
+// Safely create a directory
+const makeDir = function (dir) {
+	if (!fs.existsSync(dir)) {
+		fs.mkdirSync(dir);
+	}
 }
 
-filepaths.forEach(file => {
-    let dir = file.split('/');
-    dir.pop();
-    dir = dir.join('/');
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
-    }
-    fs.closeSync(fs.openSync(file, 'w'));    
-    console.log('✅  Created: ', file);
+// Make destination dir if it doesn't exit
+makeDir(program.destination)
+
+// For all files, create their new folders and 0 byte placeholders
+const filepaths = walkSync(basedir);
+filepaths.forEach(([dir, file]) => {
+	const path = program.destination + dir;
+	const filepath = path + '/' + file;
+	makeDir(path);
+	fs.closeSync(fs.openSync(filepath, 'w'));
+	console.log('✅  Created: ', filepath);
 });
